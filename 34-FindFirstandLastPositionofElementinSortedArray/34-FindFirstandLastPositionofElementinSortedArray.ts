081305b7477b82003bb8b9ac1525eb694dc1bc7f
Anmol Dobariya <134634201+anmoldobariya@users.noreply.github.com>
2026-06-29 22:31:49 +0530

    File updated at 6/29/2026, 10:31:49 PM

--- 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.ts
+++ 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.ts
@@ -1,31 +1,43 @@
-// Last updated: 6/29/2026, 9:55:11 PM
-1function searchRange(nums: number[], target: number): number[] {
-2    const findIndex = (checkLeft: boolean): number => {
-3        let left = 0;
-4        let right = nums.length - 1;
-5        let result = -1;
-6
-7        while (left <= right) {
-8            const mid = left + Math.floor((right - left) / 2);
+// Last updated: 6/29/2026, 10:31:49 PM
+1class MinStack {
+2    private main: number[];
+3    private min: number[];
+4
+5    constructor() {
+6        this.main = [];
+7        this.min = [];
+8    }
 9
-10            if (nums[mid] === target) {
-11                result = mid;
+10    push(value: number): void {
+11        this.main.push(value);
 12
-13                if (checkLeft) {
-14                    right = mid - 1;
-15                } else {
-16                    left = mid + 1;
-17                }
-18
-19            } else if (nums[mid] < target) {
-20                left = mid + 1
-21            } else {
-22                right = mid - 1;
-23            }
-24        }
+13        if (!this.min.length || value <= this.min[this.min.length - 1]) {
+14            this.min.push(value);
+15        }
+16    }
+17
+18    pop(): void {
+19        const value = this.main.pop();
+20
+21        if (value === this.min[this.min.length - 1]) {
+22            this.min.pop();
+23        }
+24    }
 25
-26        return result;
-27    }
-28
-29    return [findIndex(true), findIndex(false)]
-30};
+26    top(): number {
+27        return this.main[this.main.length - 1];
+28    }
+29
+30    getMin(): number {
+31        return this.min[this.min.length - 1];
+32    }
+33}
+34
+35/**
+36 * Your MinStack object will be instantiated and called as such:
+37 * var obj = new MinStack()
+38 * obj.push(value)
+39 * obj.pop()
+40 * var param_3 = obj.top()
+41 * var param_4 = obj.getMin()
+42 */
